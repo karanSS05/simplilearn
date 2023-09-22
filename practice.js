bb35c955124f081057071db11b69f7b00f355ae6
karanSS05 <karansinghshekhawat0405@gmail.com>
2023-09-22 04:29:26 +0000

    Initial Commit

---
+++ practice.js
@@ -0,0 +1,30 @@
+// let sum =0 ; 
+// let number  = [1,2,3,4,5]; 
+// for(let i =0; i<number.length;i++){
+//     sum += number[i];
+
+// }
+// console.log('sum of thee array is ' , sum );
+
+
+// let sum =0 ; 
+// let number  = [1,2,3,4,5]; 
+//  function total(num){
+//     for(let i =0; i<number.length;i++){
+//         sum += number[i];
+    
+//     }
+//     return sum ; 
+// }
+
+//console.log(total(number)); 
+
+// for(let i =1; i<=5 ;i++){
+//     console.log(i);
+// }
+
+let names = 'Hello'; 
+function addCharacter(n){
+    return names + "!";
+}
+console.log(addCharacter(names));
